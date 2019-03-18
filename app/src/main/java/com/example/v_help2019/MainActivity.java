742b309135f35258194868a91882d70202273718
Yasir <hussainyasir23@gmail.com>
2019-03-18 20:05:35 +0530

    Buttons Added

--- app/src/main/java/com/example/v_help2019/MainActivity.java
+++ app/src/main/java/com/example/v_help2019/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.v_help2019;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent homeIntent = new Intent(MainActivity.this,HomeActivity.class);
+                startActivity(homeIntent);
+
+                finish();
+            }
+        },1000);
     }
 }
