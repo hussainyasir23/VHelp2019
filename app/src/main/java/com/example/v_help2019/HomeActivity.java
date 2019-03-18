5dc4234adf9d6e2c749836403c9d3daeeafc7395
Yasir <hussainyasir23@gmail.com>
2019-03-18 21:45:03 +0530

    pricipal photo updated

--- app/src/main/java/com/example/v_help2019/HomeActivity.java
+++ app/src/main/java/com/example/v_help2019/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.v_help2019;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class HomeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void principal(View view) {
+        Intent principalIntent = new Intent(HomeActivity.this,principal.class);
+        startActivity(principalIntent);
     }
 }
