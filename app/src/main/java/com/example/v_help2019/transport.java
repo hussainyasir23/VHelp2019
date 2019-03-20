b7e8fecc8713a1fb019907aad2c4cfabd34ec9c7
Yasir <hussainyasir23@gmail.com>
2019-03-20 10:24:05 +0530

    jntu

--- app/src/main/java/com/example/v_help2019/transport.java
+++ app/src/main/java/com/example/v_help2019/transport.java
@@ -1,7 +1,9 @@
 package com.example.v_help2019;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class transport extends Activity {
 
@@ -9,5 +11,10 @@ public class transport extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transport);
+    }
+
+    public void jntu(View view) {
+        Intent jntuIntent = new Intent(transport.this,jntu.class);
+        startActivity(jntuIntent);
     }
 }
